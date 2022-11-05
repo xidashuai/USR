adcde02766f716a1709344388b4f93139b1fa77f
xidashuai <96380981+xidashuai@users.noreply.github.com>
2022-11-06 00:22:04 +0800

    新增接口

--- packages/server/main.go
+++ packages/server/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
-func main()  {
-	r:=gin.Default()
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"qny/models"
+)
 
-	r.Run(":8080")
+func main() {
+	r := gin.Default()
+	//添加新用户
+	r.POST("/adduser", models.Createuser)
+	//加入房间开启ws
+	r.GET("/addroom", models.Addroom)
+	//更新用户名
+	r.POST("/updateusername", models.Updateusername)
+	//创建房间
+	r.POST("/createroom", models.Createroom)
+	//获取公共房间列表
+	r.GET("/getroomlist", models.Getroomlist)
+
+	runerr:=r.Run(":8080")
+	if runerr!=nil{
+		log.Println(runerr)
+	}
 }
